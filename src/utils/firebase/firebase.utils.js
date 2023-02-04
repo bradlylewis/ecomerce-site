f0974c390250aa884b3098d609cbda46bea3a0eb
Bradly Lewis <bradlylewisdev@gmail.com>
2023-02-04 13:56:53 -0500

    update firebase util to create user doc

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -37,9 +37,21 @@ const firebaseConfig = {
 
   export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, "users", userAuth.uid);
-
-    console.log(userDocRef);
-
     const userSnapshot = await getDoc(userDocRef);
-    console.log(userSnapshot);
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.log("Error creating user", error.message);
+        }
+    }
+
+    return userDocRef;
   }
