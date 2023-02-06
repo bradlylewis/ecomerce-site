931c6156ab923cb778e02a65fa643e10ecd195c1
Bradly Lewis <bradlylewisdev@gmail.com>
2023-02-05 20:44:48 -0500

    add form fields state

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -11,18 +11,25 @@ const SignUpForm = () => {
     const [ formFields, setFormFields ] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
+    console.log(formFields);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormFields({ ...formFields, [name]: value });
+    }
+
     return (
         <div>
             <h1>Sign up with your email and password</h1>
             <form onSubmit={() => {}}>
                 <label>Display Name</label>
-                <input required />
+                <input onChange={handleChange} name='displayName' value={displayName} required />
                 <label>Email</label>
-                <input type='email' required />
+                <input type='email' onChange={handleChange} name='email' value={email} required />
                 <label>Password</label>
-                <input type='password' required />
+                <input type='password' onChange={handleChange} name='password' value={password} required />
                 <label>Confirm Password</label>
-                <input type='password' required />
+                <input type='password' onChange={handleChange} name='confirmPassword' value={confirmPassword} required />
                 <button type='submit'>Sign Up</button>
             </form>
         </div>
